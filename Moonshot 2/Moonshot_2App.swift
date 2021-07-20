924ccc372217f25e55bcc236c5588d5789d6a957
Olivier Van hamme <nicefiction@gmail.com>
2021-07-20 10:34:06 +0200

    Initial Commit

---
+++ Moonshot 2/Moonshot_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_2App.swift
+//  Moonshot 2
+//
+//  Created by Olivier Van hamme on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
