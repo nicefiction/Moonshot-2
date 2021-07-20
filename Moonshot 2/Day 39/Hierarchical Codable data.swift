437ce77e73bfbfb3a8572ba5915c04ee6e83bdd4
Olivier Van hamme <nicefiction@gmail.com>
2021-07-20 21:31:24 +0200

    Completes Day 39.6 : Working with hierarchical Codable data .

---
+++ Moonshot 2/Day 39/Hierarchical Codable data.swift
@@ -0,0 +1,74 @@
+//  Hierarchical Codable data.swift
+// SOURCE : https://www.hackingwithswift.com/books/ios-swiftui/working-with-hierarchical-codable-data
+
+import SwiftUI
+
+
+struct Human: Codable {
+   
+   var name: String
+   var address: Address
+}
+
+
+struct Address: Codable {
+   
+   var street: String
+   var city: String
+}
+
+
+
+struct Hierarchical_Codable_data: View {
+   
+   // MARK: COMPUTED PROPERTIES
+   
+   var body: some View {
+      
+      Button("Decode JSON") {
+         let input =  """
+            {
+               "name": "Dorothy Gale"
+               "address": {
+                  "street": "123, Wizard street"
+                  "city": "Emerald"
+               }
+            }
+         """
+         
+         
+//         struct Human: Codable {
+//
+//            var name: String
+//            var address: Address
+//         }
+//
+//
+//         struct Address: Codable {
+//
+//            var street: String
+//            var city: String
+//         }
+         
+
+         let data = Data(input.utf8)
+         let jsonDecoder = JSONDecoder()
+         if let _human = try? jsonDecoder.decode(Human.self, from: data) {
+            print(_human.address.street)
+         }
+      }
+   }
+}
+
+
+
+
+// MARK : - PREVIEWS -
+
+struct Hierarchical_Codable_data_Previews: PreviewProvider {
+   
+   static var previews: some View {
+      
+      Hierarchical_Codable_data()
+   }
+}
